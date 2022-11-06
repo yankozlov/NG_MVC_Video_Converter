d51a452190685fa6eef787c8f4b770aeb0ad00c2
Yan Kozlov <kozlov.yan1@gmail.com>
2022-11-06 21:43:08 +0200

    Implemented file downloading and display of clients' only files

--- VideoConverter/Controllers/HomeController.cs
+++ VideoConverter/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using VideoConverter.Models;
@@ -25,17 +26,72 @@ namespace VideoConverter.Controllers
         }
 
         [HttpPost]
-        public IActionResult UploadPage(IFormFile input_file)
+        public IActionResult UploadPage(IFormFile inputFile)
         {
-            if (input_file == null)
+            if (inputFile != null)
             {
-                return RedirectToAction("Index");
+                //?
+                var fileName = Path.GetFileName(inputFile.FileName);
+
+                var filePath = Path.Combine(Directory.GetCurrentDirectory(), "Uploads", fileName);
+
+                if (System.IO.File.Exists(filePath))
+                {
+                    System.IO.File.Delete(filePath);
+                }
+
+                using (var localFile = System.IO.File.OpenWrite(filePath))
+                using (var uploadedFile = inputFile.OpenReadStream())
+                {
+                    uploadedFile.CopyTo(localFile);
+                }
             }
 
-            FileModel model = new FileModel { Name = input_file.FileName };
+            ViewBag.Message = "File is successfully uploaded";
+
+            var item = Directory.GetFiles(Path.Combine(Directory.GetCurrentDirectory(), "Uploads"))
+                                .Where(f => System.IO.Path.GetFileName(f) == inputFile.FileName)
+                                .FirstOrDefault();
+            var model = new FileModel { Name = System.IO.Path.GetFileName(item), Path = item };
 
             return View(model);
         }
+
+        public async Task<IActionResult> Download(string filename)
+        {
+            if (filename == null)
+                return Content("Filename is not available");
+
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "Uploads", filename);
+
+            var memory = new MemoryStream();
+            using (var stream = new FileStream(path, FileMode.Open))
+            {
+                await stream.CopyToAsync(memory);
+            }
+            memory.Position = 0;
+            return File(memory, GetContentType(path), Path.GetFileName(path));
+        }
+
+        // Get content type
+        private string GetContentType(string path)
+        {
+            var types = GetMimeTypes();
+            var ext = Path.GetExtension(path).ToLowerInvariant();
+            return types[ext];
+        }
+
+        private Dictionary<string, string> GetMimeTypes()
+        {
+            return new Dictionary<string, string>
+            {
+                {".avi", "video/x-msvideo"},
+                {".mp4", "video/mp4"},
+                {".mpeg", "video/mpeg"},
+                {".webm", "video/webm"}
+            };
+        }
+        //!!!!!
 
         public IActionResult Privacy()
         {
--- VideoConverter/Models/FileModel.cs
+++ VideoConverter/Models/FileModel.cs
@@ -6,5 +6,6 @@ namespace VideoConverter.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public string Path { get; set; }
     }
 }
