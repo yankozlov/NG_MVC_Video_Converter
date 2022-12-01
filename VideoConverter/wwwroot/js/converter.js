4279d2cb7ccedb2c2a101e2056f6e9139ef77920
Yan Kozlov <kozlov.yan1@gmail.com>
2022-12-02 01:50:55 +0200

    Implemented frontend

---
+++ VideoConverter/wwwroot/js/converter.js
@@ -0,0 +1,151 @@
+﻿$(function () {
+
+    var ul = $('#convert-status ul');
+    
+    ul.find("li").each(function () {
+
+        let li = $(this);
+
+        fileModelList.find(x => x.name === li.attr("id")).status = "converting";
+        let model = fileModelList.find(x => x.name === li.attr("id"));
+
+        li.find('input').knob();
+        li.find('input').val(33).change();
+
+        li.find('span').click(function () {
+            //if (li.hasClass('working')) {
+            //    jqXHR.abort();
+            //}
+
+            li.fadeOut(function () {
+                for (var i = 0; i < fileModelList.length; i += 1) {
+                    if (fileModelList[i].name == li.attr("id")) {
+                        fileModelList.splice(i, 1);
+                        break;
+                    }
+                }
+
+                li.remove();
+
+                if (fileModelList.length == 0) {
+                    console.log("redirect to index");
+                    window.location.replace(window.location.origin);
+                }
+                else {
+                    console.log(fileModelList.filter(x => x.status !== "done").length);
+                    console.log($('#download-button').prop('disabled'));
+
+                    if (fileModelList.filter(x => x.status !== "done").length == 0 &&
+                        $('#download-button').prop('disabled') == true) {
+
+                        $('#download-button').prop('disabled', false);
+                    }
+                }
+            });
+
+        });
+
+        $.ajax({
+            type: "POST",
+            url: "/Home/Convert",
+            data: {
+                id: model.id,
+                name: model.name,
+                extension: model.extension,
+                status: model.status
+            },
+            success: function (filename) {
+                console.log("success | add downlaod on click");
+                li.attr("id", filename);
+
+                let p_text = li.find('p').html().split('\n');
+                p_text[0] = filename;
+
+                li.find('p').html(p_text.join('\n')).change();
+
+                li.find('input').val(100).change();
+                
+                fileModelList.find(x => x.name === model.name).name = filename;
+                fileModelList.find(x => x.name === filename).status = "done";
+
+                if (fileModelList.filter(x => x.status !== "done").length == 0 &&
+                    $('#download-button').prop('disabled') == true) {
+
+                    $('#download-button').prop('disabled', false);
+                }
+            }
+        });
+    });
+
+    $('#download-button').click(function () {
+        for (const fileModel of fileModelList.filter(x => x.status === "done")) {
+            $.ajax({
+                type: "POST",
+                url: "/Home/Download",
+                data: {
+                    filename: fileModel.name
+                },
+                xhrFields: {
+                    responseType: 'blob' // to avoid binary data being mangled on charset conversion
+                },
+                async: true,
+                success: function (blob, status, xhr) {
+
+                    // check for a filename
+                    var filename = "";
+                    var disposition = xhr.getResponseHeader('Content-Disposition');
+                    if (disposition && disposition.indexOf('attachment') !== -1) {
+                        var filenameRegex = /filename[^;=\n]*=((['"]).*?\2|[^;\n]*)/;
+                        var matches = filenameRegex.exec(disposition);
+                        if (matches != null && matches[1]) filename = matches[1].replace(/['"]/g, '');
+                    }
+
+                    if (typeof window.navigator.msSaveBlob !== 'undefined') {
+                        // IE workaround for "HTML7007: One or more blob URLs were revoked by closing the blob for which they were created. These URLs will no longer resolve as the data backing the URL has been freed."
+                        window.navigator.msSaveBlob(blob, filename);
+                    } else {
+                        var URL = window.URL || window.webkitURL;
+                        var downloadUrl = URL.createObjectURL(blob);
+
+                        if (filename) {
+                            // use HTML5 a[download] attribute to specify filename
+                            var a = document.createElement("a");
+                            // safari doesn't support this yet
+                            if (typeof a.download === 'undefined') {
+                                window.location.href = downloadUrl;
+                            } else {
+                                a.href = downloadUrl;
+                                a.download = filename;
+                                document.body.appendChild(a);
+                                a.click();
+                            }
+                        } else {
+                            window.location.href = downloadUrl;
+                        }
+
+                        setTimeout(function () { URL.revokeObjectURL(downloadUrl); }, 100); // cleanup
+                    }
+                    $(`li[id="${filename}"] span`).click();
+                }
+            });
+        };
+    });
+
+    // Helper function that formats the file sizes
+    function formatFileSize(bytes) {
+        if (typeof bytes !== 'number') {
+            return '';
+        }
+
+        if (bytes >= 1000000000) {
+            return (bytes / 1000000000).toFixed(2) + ' GB';
+        }
+
+        if (bytes >= 1000000) {
+            return (bytes / 1000000).toFixed(2) + ' MB';
+        }
+
+        return (bytes / 1000).toFixed(2) + ' KB';
+    }
+
+});
---
+++ VideoConverter/wwwroot/js/index.js
@@ -0,0 +1,150 @@
+var fileList = [];
+
+$(function () {
+
+    var ul = $('#upload ul');
+
+    $('#drop a').click(function(){
+        // Simulate a click on the file input button
+        // to show the file browser dialog
+        $(this).parent().find('input').click();
+    });
+
+    $('#convert').on('submit', function () {
+
+        $('#fileList').val(JSON.stringify(fileList));
+        console.log($('#fileList').val());
+
+        return true;
+    });
+
+    // Initialize the jQuery File Upload plugin
+    $('#upload').fileupload({
+
+        // This element will accept file drag/drop uploading
+        dropZone: $('#drop'),
+
+        // This function is called when a file is added to the queue;
+        // either via the browse button, or via drag/drop:
+        add: function (e, data) {
+
+            var tpl = $('<li class="working"><input type="text" value="0" data-width="48" data-height="48"'+
+                ' data-fgColor="#0788a5" data-readOnly="1" data-bgColor="#3e4043" /><p></p><div class="settings">'+
+                '<label>Format: </label><select name="format" class="format-select"><option value="avi">avi</option>'+
+                '<option value="mp4">mp4</option><option value="mov">mov</option></select></div><span></span></li>');
+
+            // Append the file name and file size
+            //var filename = data.files[0].name;
+            tpl.find('p').text(data.files[0].name)
+                         .append('<i>' + formatFileSize(data.files[0].size) + '</i>');
+
+            // Add the HTML to the UL element
+            data.context = tpl.appendTo(ul);
+
+            // Initialize the knob plugin
+            tpl.find('input').knob();
+
+            // Listen for clicks on the cancel icon
+            tpl.find('span').click(function(){
+
+                if(tpl.hasClass('working')){
+                    jqXHR.abort();
+                }
+
+                tpl.fadeOut(function () {
+                    fileList = removeFileListItem(fileList, data.files[0].name);
+
+                    tpl.remove();
+
+                    if (fileList.length == 0) {
+                        $('#convert-button').prop('disabled', true);
+                    }
+                });
+
+            });
+
+            // Automatically upload the file once it is added to the queue
+            var jqXHR = data.submit();
+        },
+
+        progress: function(e, data){
+
+            // Calculate the completion percentage of the upload
+            var progress = parseInt(data.loaded / data.total * 100, 10);
+
+            // Update the hidden input field and trigger a change
+            // so that the jQuery knob plugin knows to update the dial
+            data.context.find('input').val(progress).change();
+
+            if(progress == 100){
+                data.context.removeClass('working');
+                var select = data.context.find('.format-select option:selected').text();
+                var fileName = data.files[0].name;
+
+                fileList.push({
+                    key: fileName,
+                    value: select
+                });
+
+                if ($('#convert-button').prop('disabled') == true) {
+                    $('#convert-button').prop('disabled', false);
+                }
+            }
+        },
+
+        fail:function(e, data){
+            // Something has gone wrong!
+            data.context.addClass('error');
+
+            delete fileList[data.files[0].name];
+
+            if (fileList.length == 0) {
+                $('#convert-button').prop('disabled', true);
+            }
+
+            setTimeout(() => {
+                const elem = data.context;
+                elem.remove();
+            }, 2000);
+        }
+
+    });
+
+
+    // Prevent the default action when a file is dropped on the window
+    $(document).on('drop dragover', function (e) {
+        e.preventDefault();
+    });
+
+    // Function to normalize interface after fileList' length change
+
+    // Helper function to remove item from fileList
+    function removeFileListItem(list, item) {
+        for (var i = 0; i < list.length; i += 1) {
+            if (list[i].key == item) {
+                list.splice(i, 1);
+                break;
+            }
+        }
+        return list;
+    }
+
+
+    // Helper function that formats the file sizes
+    function formatFileSize(bytes) {
+        if (typeof bytes !== 'number') {
+            return '';
+        }
+
+        if (bytes >= 1000000000) {
+            return (bytes / 1000000000).toFixed(2) + ' GB';
+        }
+
+        if (bytes >= 1000000) {
+            return (bytes / 1000000).toFixed(2) + ' MB';
+        }
+
+        return (bytes / 1000).toFixed(2) + ' KB';
+    }
+
+});
